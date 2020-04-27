dece3ca1ba1048c805444fb7475d1205e3709677
Mauricio Viera Sotillo <mauricio.viera6@gmail.com>
2020-04-27 13:48:16 +0200

    position

---
+++ main.js
@@ -0,0 +1,7 @@
+// function navOpen() {
+//   const elem = (s) => document.getElementsByClassName(s);
+//   elem("nav-menu").setAttribute();
+// }
+
+// const elem = (s) => document.getElementsByClassName(s);
+// console.log(elem("nav-menu"));
