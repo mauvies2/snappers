5515f6d59a7abb2c1bdc1ef9fd03d5f66047312a
Mauricio Viera Sotillo <mauricio.viera6@gmail.com>
2020-04-27 22:35:18 +0200

    commit

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-// function navOpen() {
-//   const elem = (s) => document.getElementsByClassName(s);
-//   elem("nav-menu").setAttribute();
-// }
+function openNav() {
+  document.getElementById("nav-menuu").classList.add("open");
+}
+function closeNav() {
+  document.getElementById("nav-menuu").classList.remove("open");
+}
 
-// const elem = (s) => document.getElementsByClassName(s);
-// console.log(elem("nav-menu"));
+function Lang() {
+  const element = document.getElementById("drop-list");
+  if (element.classList != "dropdown-list openLangu") {
+    element.classList.add("openLangu");
+  } else {
+    element.classList.remove("openLangu");
+  }
+}
+
+// function closeLang() {
+//   const newLocal = "dropdown-list openLangu";
+//   if (document.getElementById("drop-list").classList[1] === openLangu) {
+//     document.getElementById("drop-list");
+//   }
+// }
